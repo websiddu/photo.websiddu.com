b5bb525dfd055ddaf3100f059494339af171809e
Siddhartha <gsid@google.com>
2016-12-30 19:46:28 -0800

    Adding the back to top button

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -90,7 +90,7 @@
 
 jQuery(document).ready(function($){
   // browser window scroll (in pixels) after which the "back to top" link is shown
-  var offset = 300,
+  var offset = 2000,
     //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
     offset_opacity = 1200,
     //duration of the top scrolling animation (in ms)
